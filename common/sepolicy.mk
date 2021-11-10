751e42b6afb5acc60096da60bddc8d31c73cf44a
Michael Bestas <mkbestas@gmail.com>
2021-11-10 23:32:44 +0200

    Remove legacy sepolicy leftover
    Change-Id: I2913565cc402b29acd380015b049ccf8d4e9959d

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -30,9 +30,6 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
 
 endif
 
-# Selectively include legacy rules defined by the products
--include device/pixelstar/sepolicy/legacy-common/sepolicy.mk
-
 # Include atv rules on atv product
 ifeq ($(PRODUCT_IS_ATV), true)
 include device/pixelstar/sepolicy/atv/sepolicy.mk

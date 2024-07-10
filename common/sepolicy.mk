55cd55529b6864ccdbc1182d9d871e070f53b345
Mudit200408 <muditpurohit2004.pm@gmail.com>
2024-07-10 13:07:34 +0530

    Revert "sepolicy: include flipendo/turbo_adapter policy"
    This reverts commit 6205ab2d06a139711ee1473fcb048426b745531e.

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -18,16 +18,11 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     device/pixelstar/sepolicy/common/dynamic \
-    device/pixelstar/sepolicy/common/system \
-    hardware/google/pixel-sepolicy/flipendo \
-    hardware/google/pixel-sepolicy/turbo_adapter
+    device/pixelstar/sepolicy/common/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/pixelstar/sepolicy/common/dynamic \
-    device/pixelstar/sepolicy/common/vendor \
-    hardware/google/pixel-sepolicy/flipendo \
-    hardware/google/pixel-sepolicy/turbo_adapter
-
+    device/pixelstar/sepolicy/common/vendor
 endif
 
 # Include atv rules on atv product
